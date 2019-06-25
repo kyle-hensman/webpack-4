b56590ca1eb86a67b565cf857b3bf461ba2defb7
EliteBook <kylehensman@gmail.com>
2019-06-25 11:39:05 -0300

    added webpack cleaner plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
     app: './src/index.js',
-    print: './src/print.js',
+    print: './src/print.js'
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'Output Management'
     })
