d79db54f68b611f956d2610a25e2142970ec76a2
EliteBook <kylehensman@gmail.com>
2019-06-25 11:33:15 -0300

    added webpack template examples

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,60 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: require('html-webpack-template'),
+      appMountId: 'app',
+      appMountHtmlSnippet: '<div class="app-spinner"><i class="fa fa-spinner fa-spin fa-5x" aria-hidden="true"></i></div>',
+      headHtmlSnippet: '<style>div.app-spinner {position: fixed;top:50%;left:50%;}</style >',
+      bodyHtmlSnippet: '<custom-element></custom-element>',
+      // baseHref: 'http://example.com/awesome',
+      // devServer: 'http://localhost:3001',
+      googleAnalytics: {
+        trackingId: 'UA-XXXX-XX',
+        pageViewOnLoad: true
+      },
+      meta: [
+        {
+          name: 'description',
+          content: 'A better default template for html-webpack-plugin.'
+        }
+      ],
+      mobile: true,
+      lang: 'en-US',
+      links: [
+        'https://fonts.googleapis.com/css?family=Roboto',
+        {
+          href: '/apple-touch-icon.png',
+          rel: 'apple-touch-icon',
+          sizes: '180x180'
+        },
+        {
+          href: '/favicon-32x32.png',
+          rel: 'icon',
+          sizes: '32x32',
+          type: 'image/png'
+        }
+      ],
+      inlineManifestWebpackName: 'webpackManifest',
+      // scripts: [
+      //   'http://example.com/somescript.js',
+      //   {
+      //     src: '/myModule.js',
+      //     type: 'module'
+      //   }
+      // ],
+      title: 'Output Management'
+    })
+  ],
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   }
 };
